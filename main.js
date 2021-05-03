75d76e0b8e450628f3950b58ed5f12952ea93e4e
devder <devder@umich.edu>
2021-05-02 22:51:03 -0400

    Changed files: main.js
    Added Changes:
    main.js:
    	Finished the functionality of the num to string conversion. Have
    	not implemented the argument part of the problem. May turn code
    	into a function for better use later.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 
-var arr = [];
+
+var arr = [3, 25, 209];
 var arrNum;
 
 var nameObj = {
@@ -16,9 +17,17 @@ var nameObj = {
   "0": "Zero"
 };
 
+var outputString = "";
 for (var i=0; i<arr.length; ++i) {
+  var outputAddon = "";
   arrNum = arr[i];
   while (!(arrNum === 0)) {
-    
+    outputAddon = nameObj[(arrNum % 10) + ""] + outputAddon;
+    arrNum = Math.floor(arrNum/10);
+  }
+  outputString += outputAddon;
+  if (!(i === arr.length-1)) {
+    outputString += ",";
   }
 }
+console.log(outputString);
