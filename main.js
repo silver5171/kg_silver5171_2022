a2ed8548ea648dd60b06a698feea6f51445a4fc2
devder <devder@umich.edu>
2021-05-02 22:33:38 -0400

    Initial commit. Included: main.js
    main.js has the beginnings of a solution to the Kargo 2021 Internship Question. The main thing
    added is an Object that will contain how I will convert the numbers into strings.

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+var arr = [];
+var arrNum;
+
+var nameObj = {
+  "1": "One",
+  "2": "Two",
+  "3": "Three",
+  "4": "Four",
+  "5": "Five",
+  "6": "Six",
+  "7": "Seven",
+  "8": "Eight",
+  "9": "Nine",
+  "0": "Zero"
+};
+
+for (var i=0; i<arr.length; ++i) {
+  arrNum = arr[i];
+  while (!(arrNum === 0)) {
+    
+  }
+}
